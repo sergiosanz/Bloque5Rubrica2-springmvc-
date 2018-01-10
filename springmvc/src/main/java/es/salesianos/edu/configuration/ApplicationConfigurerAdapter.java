a2770002cf0ee02099db0fabc0577ff20f84f6db
sergiosanz <sanzestradasergio@gmail.com>
2018-01-10 09:14:57 +0100

    Done(x3)

---
+++ springmvc/src/main/java/es/salesianos/edu/configuration/ApplicationConfigurerAdapter.java
@@ -0,0 +1,29 @@
+package es.salesianos.edu.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.technicalkeeda")
+public class ApplicationConfigurerAdapter extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+}
---
+++ springmvc/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>Spring 4 Hello World Example!!</title>
+ </head>
+ <body>
+     <h1>Hello ${name}</h1>
+ </body>
+</html>
